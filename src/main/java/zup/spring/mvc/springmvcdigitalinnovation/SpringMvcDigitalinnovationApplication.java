6c525e3c9750d3be40ebf678777ef408ba48c85f
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-08-15 11:49:26 -0300

    Testando no Heroku

---
+++ src/main/java/zup/spring/mvc/springmvcdigitalinnovation/SpringMvcDigitalinnovationApplication.java
@@ -0,0 +1,13 @@
+package zup.spring.mvc.springmvcdigitalinnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDigitalinnovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDigitalinnovationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/spring/mvc/springmvcdigitalinnovation/SpringMvcDigitalinnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.spring.mvc.springmvcdigitalinnovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDigitalinnovationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
